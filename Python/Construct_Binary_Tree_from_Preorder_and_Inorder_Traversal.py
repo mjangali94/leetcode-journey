39882927472a82fd1377c99fda0cc8a11afce3c5
mjangali94@gmail.com <mjangali94@gmail.com>
2025-12-07 19:12:04 -0500

    More optimized version of last challenges

--- Python/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ Python/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -26,3 +26,31 @@ class Solution:
         head.right = self.buildTree(preorder[index + 1:], inorder[index + 1:])
 
         return head
+
+
+
+
+
+
+# class Solution:
+#     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        
+#         index_map = {val: i for i, val in enumerate(inorder)}
+#         self.post_idx = len(postorder) - 1
+#         def build(left, right):
+
+#             if left > right:
+#                 return None
+
+#             root_val = postorder[self.post_idx]
+#             self.post_idx -= 1
+
+#             root = TreeNode(root_val)
+#             mid = index_map[root_val]
+            
+#             root.right = build(mid + 1, right)
+#             root.left = build(left, mid - 1)
+
+#             return root
+
+#         return build(0, len(inorder) - 1)
